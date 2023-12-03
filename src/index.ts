f368c12b4a00a55094afc8e44516b9218f819bfa
CapSap <cshotam@gmail.com>
2023-12-03 14:29:51 +1100

    intial code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello test");
+let counter = 0;
+const button = document.querySelector(".button");
+console.log("button", button);
+
+if (button) {
+  button.addEventListener("click", () => {
+    console.log("click");
+  });
+}
+
+function handleClick() {
+  console.log("click");
+}
+
+const button2 = document.getElementById("button");
+
+console.log("but2", button2);
